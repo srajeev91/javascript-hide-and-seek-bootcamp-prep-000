7035d36939031e70e586e3493a0b8f749b4a0d92
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-28 23:52:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < ranks.length; i++) {
-    
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n);
   }
 }

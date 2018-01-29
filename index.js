112d208f64d9522d6ddb495415a18d99d6592a41
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-29 00:30:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  const idNested = document.getElementById('nested');
+  return idNested.getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n);
+  }
+}
+
+function deepestChild() {
+  var grandNode = document.getElementById('grand-node');
+  var theChild = grandNode.children[0];
+  
+  while(theChild) {
+    grandNode = theChild;
+    theChild = grandNode.children[0];
+  }
+return grandNode;
+}
